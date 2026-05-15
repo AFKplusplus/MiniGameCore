5ba7607cdb7ba0d49f1ea308c0d9a727d4e58975
AFKplusplus <affankazuo@gmail.com>
2026-05-15 19:21:46 +0800

    Renamed folder to ORE and made Party.kt

--- src/main/java/ore/MiniGameCore/utils/LobbyHandler.kt
+++ src/main/java/ore/MiniGameCore/utils/LobbyHandler.kt
@@ -17,9 +17,14 @@ import java.util.Objects
 import java.nio.file.Files.move
 
 public class LobbyHandler {
+
+    // This is to make the variables and functions static
     companion object {
+
+        // Initialize the plugin variable
         private var plugin: MiniGameCore
 
+        // Resets the specified lobby
         public fun LobbyReset(lobby: Lobby) {
             if (lobby == null) {
                 getLogger().warning("The lobby does not exist!")
@@ -29,35 +34,61 @@ public class LobbyHandler {
             removeLobby(lobby.getLobbyID())
         }
 
+        // Deletes the world folder of the specified lobby
         private fun deleteWorldFolder(lobby: Lobby) {
+
+            // Initialize the world folder name and the world
             var name: String = lobby.getWorldFolder().getName()
             var world: World = Bukkit.getWorld(name)
 
+            // Return if the world specified is null
             if (world != null) {
+
+                // For every player in the world, teleport them back to spawn
                 for (player: Player in world.getPlayers()) {
                     player.teleport(Objects.requireNonNull(Bukkit.getWorld("world")).getSpawnLocation())
                 }
+
+                // Unload the world
                 var unloaded: Boolean = Bukkit.unloadWorld(world, false)
+
+                // If the world failed to unload warn the console
                 if (!unloaded) {
                     plugin.getLogger().warning("Failed to unload world: $world")
                     return
+
                 }
             } else return
+
+            // If the config is set to keep the worlds, archive the world folders in the archivedWorlds folder
             if (plugin.getKeepWorlds()) {
+
+                // Create the archived worlds folder
                 var archivedDirectory: File = File(plugin.getDataFolder(), "archivedWorlds")
+
+                // If the folder still doesnt exist and it failed to create warn the console
                 if (!archivedDirectory.exists() && !archivedDirectory.mkdirs()) {
                     plugin.getLogger().severe("Failed to create archive directory: ${archivedDirectory.getPath()}")
                     return
                 }
+
                 try {
+
+                    // Try to move the world folders into the archived wolrds folder
                     move(lobby.getWorldFolder().toPath(), archivedDirectory.toPath())
-                    plugin.getLogger().infO("Archived world: $name")
+                    plugin.getLogger().info("Archived world: $name")
+
                 } catch (e: IOException) {
+
+                    // Failed, warn the console and print the stack trace
                     plugin.getLogger().severe("Failed to archive world: $name")
                     e.printStackTrace()
+
                 }
                 return
             }
+
+            // Attempt to delete the world folder, if it fails then warn the console
             if (delete(lobby.getWorldFolder())) {
                 plugin.getLogger().info("Deleted world folder: $name")
             } else {
@@ -65,15 +96,21 @@ public class LobbyHandler {
             }
         }
 
-        private fun delete(folder: File): Boolean {
-            if (folder.isDirectory()) {
-                for (child: File in Objects.requireNonNull(folder.listFiles())) {
+        // Delete a file/directory
+        private fun delete(file: File): Boolean {
+
+            // If the file is a directory, delete all the children files
+            if (file.isDirectory()) {
+                for (child: File in Objects.requireNonNull(file.listFiles())) {
                     delete(child)
                 }
             }
+
+            // Then delete the file/directory itself
             return file.delete()
         }
 
+        // Set the plugin variable
         public fun setPlugin(plugin: MiniGameCore) {
             LobbyHandler.plugin = plugin
         }
---
+++ src/main/java/ore/MiniGameCore/utils/Party.kt
@@ -0,0 +1,100 @@
+package ore.MiniGameCore.utils
+
+// Bukkit imports
+import org.bukkit.entity.Player
+
+// Java imports
+import java.util.HashSet
+
+public class Party {
+
+    // Initialize variables
+    private val partyID: String
+    private val partyName: String
+    private val owner: Player
+    private val players: HashSet<Player> = HashSet()
+    private val playersInvited: HashSet<Player> = HashSet()
+
+    // Class constructor function
+    public fun Party(partyID: String, partyName: String, owner: Player) {
+        this.partyID = partyID
+        this.partyName = partyName
+        this.owner = owner
+        this.players.add(owner)
+    }
+
+    // Add player into a party
+    public fun addPlayer(player: Player): Boolean {
+        
+        // If the player is already in the party return
+        if (players.contains(player)) return false
+
+        // If the player wasn't invited return
+        if (!playersInvited.contains(player)) return false
+
+        // Remove the player from the invited list and add them to the party
+        playersInvited.remove(player)
+        return players.add(player)
+
+    }
+
+    // Remove the player from a party
+    public fun removePlayer(player: Player): Boolean {
+        return players.remove(player)
+    }
+
+    // Invite a player into a party
+    public fun invitePlayer(player: Player): Boolean {
+
+        // If the player is already in the party return
+        if (players.contains(player)) return false
+
+        // If the player was already invited return
+        if (playersInvited.contains(player)) return false
+
+        // Invite the player
+        return playersInvited.add(player)
+
+    }
+
+    // Returns whether the player is in a party
+    public fun containsPlayer(player: Player): Boolean {
+        return players.contains(player)
+    }
+
+    // Returns whether the palyer was invited to a party
+    public fun isInvited(player: Player): Boolean {
+        return playersInvited.contains(player)
+    }
+
+    // Get the list of players in a party
+    public fun getPlayers(): HashSet<Player> {
+        return players
+    }
+
+    // Get the party ID
+    public fun getPartyID(): String {
+        return partyID
+    }
+
+    // Get the party name
+    public fun getPartyName(): String {
+        return partyName
+    }
+
+    // Returns whether a player is the owner of a party
+    public fun isOwner(player: Player): Boolean {
+        return owner == player
+    }
+
+    // Gets the owner of a party
+    public fun getOwner(): Player {
+        return owner
+    }
+
+    // Run if the player denies an invite
+    public fun denyInvite(player: Player): Boolean {
+        return playersInvited.remove(player)
+    }
+    
+}
